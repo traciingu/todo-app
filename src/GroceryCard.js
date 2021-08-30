9ee44175ff8e20e70ced0f126dcd11b246eb80f3
Tracy Nguyen <traciinguyen@hotmail.com>
2021-08-30 14:34:46 -0400

    Created GroceryCard

---
+++ src/GroceryCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function GroceryCard(){
+    return(
+        <div>
+            <h1>Grocery List</h1>
+            <input type="text"/> <button>Submit</button>
+        </div>
+    );
+}
