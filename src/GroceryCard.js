2a085424e3fce396d0295e60fb517471b04b35b2
Tracy Nguyen <traciinguyen@hotmail.com>
2021-08-30 18:00:55 -0400

    Add state and submit functionality

--- src/GroceryCard.js
+++ src/GroceryCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
 export default function GroceryCard(){
+    const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setList([...list, name])
+    }
+
     return(
         <div>
             <h1>Grocery List</h1>
-            <input type="text"/> <button>Submit</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e) => setName(e.target.value)}/>
+                <button type="submit">Submit</button>
+            </form>
+            {list.map(item => <p>{item}</p>)}
         </div>
     );
 }
