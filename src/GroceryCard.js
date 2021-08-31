b1f0fe01dbe647ef66ff07d21e3c7f6889b95b2b
Tracy Nguyen <traciinguyen@hotmail.com>
2021-08-31 16:21:03 -0400

    Created "Clear Items" btn & stylized Grocery Card

--- src/GroceryCard.js
+++ src/GroceryCard.js
@@ -1,22 +1,27 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import './style/GroceryCard.css';
 
-export default function GroceryCard(){
+export default function GroceryCard() {
     const [name, setName] = useState("");
     const [list, setList] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setList([...list, name])
+        setName("");
+        setList([...list, name]);
     }
 
-    return(
-        <div>
+    return (
+        <div className="grocery-card">
             <h1>Grocery List</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) => setName(e.target.value)}/>
-                <button type="submit">Submit</button>
-            </form>
-            {list.map(item => <p>{item}</p>)}
+            {/* <div className="inner-grocery-card"> */}
+                <form className="create-item-form" onSubmit={handleSubmit}>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    <button type="submit">Submit</button>
+                </form>
+                {list.map(item => <p style={{width: "50%", margin: ".2em 0em", padding: ".2em 0em", paddingLeft: "1em", backgroundColor: "#fffef0", borderRadius: ".15em" }}>{item}</p>)}
+                <button onClick={() => setList([])} style={{ margin: "1.5em 0em" }}>Clear Items</button>
+            {/* </div> */}
         </div>
     );
 }
